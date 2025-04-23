a66548eb1a33278f92b61d53992f3f25a09e34d7
Mathew Vieira <mvieirabezerra@gmail.com>
2025-04-23 11:52:31 -0300

    feat/mathew

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ function ehNumero(n) {
   return true
 }
 
+function valorPermitido(valor, valoresPermitidos, habilitarAlert = true) {
+  if (!valoresPermitidos.includes(valor)) {
+    if (habilitarAlert) alert('Valor não permitido! Tente novamente.')
+    return false
+  }
+  return true
+}
+
 function q01() {
   const produtoA = 10
   const produtoB = 5
@@ -49,4 +57,27 @@ function q04() {
       .map(([operacao, resultado]) => `"${valorA}" ${operacao} ${valorB} = ${resultado}`)
       .join('\n')
   )
+}
+
+function q05() {
+  const ehColaborador = prompt('O colaborador está ativo? (y/n)').trim().toLowerCase()
+
+  if (!valorPermitido(ehColaborador, ['y', 'n'])) return
+
+  alert(`Benefício ${ehColaborador === 'y' ? 'negado' : 'liberado'}!`)
+}
+
+function q06() {
+  const nota = Number.parseFloat(prompt('Informe a nota do aluno'))
+
+  if (ehNumero(nota)) alert(`Aluno ${nota >= 6.0 && nota <= 10.0 ? 'aprovado' : 'reprovado'}!`)
+}
+
+function q07() {
+  const nome = prompt('Informe o nome do convidado').trim().toLowerCase()
+
+  const convidados = ['luis', 'alex', 'carlos']
+  const vips = ['carl', 'alice']
+
+  valorPermitido(nome, convidados.concat([vips]), false) ? alert('Pode passar!') : alert('Vá embora!')
 }

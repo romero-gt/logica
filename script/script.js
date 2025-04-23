abff77eabe8e28da3434290115289273d2738861
Olavo Vieira <olavovieira203@gmail.com>
2025-04-23 12:02:45 -0300

    feat: 4 desafios js

--- script/script.js
+++ script/script.js
@@ -1,17 +1,3 @@
-// let produto1 = 10;
-// let produto2 = 5;
-// let produto2Dobrado = produto2 * 2;
-// let total = produto1 + produto2Dobrado;
-
-// let mensagem;
-// if (total > 15) {
-//     mensagem = `O total é ${total}, que ultrapassa o valor de R$15.`;
-// }
-// else {
-//     mensagem = `O total é ${total}, não ultrapassa o valor de R$15.`
-// }
-// document.getElementById("resultado").textContent = mensagem
-
 function question01() {
     let produto1 = Number(prompt("Digite uma valor: "));
     let produto2 = Number(prompt("Digite segundo valor: "));
@@ -23,4 +9,33 @@ function question01() {
     else {
         alert (`O total é ${total}, não ultrapassa o valor de R$15.`);
     }
+}
+
+function question02() {
+    let numero = Number(prompt("Dígite um número: "))
+    let parImpar = (numero % 2 == 0) ? "par" : "impar";
+
+    alert(`O seu número é ${parImpar}`)
+}
+
+function question03() {
+    let idade = Number(prompt("Qual sua idade: "))
+    let carteira = prompt("Possui CNH valida? 'Sim' ou 'Não': ")
+
+    if (idade >= 25 && carteira == "Sim" || carteira == "sim") {
+        alert(`Você está dentro das normas para alugar um veículo!`)
+    }
+    else {
+        alert(`Infelizmente você não está dentro das normas para alugar um carro!`)
+    }
+}
+
+function question04() {
+    let a = "10"
+    let b = 10
+    let resultado1 = a === b
+    let resultado2 = a == b
+
+    alert(`"${a}" é estritamente igual a ${b}? O resultado é ${resultado1}. "${a}" é uma strig e o ${b} é um número! \n
+        E "${a}" é igual a ${b}? O resultado é ${resultado2}. Os dois são iguais!`)
 }
